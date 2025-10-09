80d9357b8eea968f68004e0b46754cc0c5766a2e
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-10-09 15:53:24 +0300

    Add test for daysUntilChristmas

--- test/untestable1.test.mjs
+++ test/untestable1.test.mjs
@@ -1,10 +1,10 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
-import { daysUntilChristmas } from "../src/untestable1.mjs";
+import { daysUntilChristmas } from "../src/untestable1_copy.ts";
 
 describe("Untestable 1: days until Christmas", () => {
-  test("todo", () => {
-    // TODO: write proper tests
-    expect(daysUntilChristmas()).to.be.a("number");
+  test("should be 24 from December 1st", () => {
+    const dec1 = new Date(2000, 12 - 1, 1);
+    expect(daysUntilChristmas(dec1)).to.equals(24);
   });
 });
