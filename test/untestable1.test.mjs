e76c183c794265832bf9d3015cd4733fd8aaac44
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-10-09 16:26:45 +0300

    Add test for daysUntilChristmas. Fix a bug where new Date was used instead of given now

--- test/untestable1.test.mjs
+++ test/untestable1.test.mjs
@@ -13,8 +13,18 @@ describe("Untestable 1: days until Christmas", () => {
     expect(daysUntilChristmas(nov1)).to.equals(54);
   });
 
-  test("should be 0 from December 25 when", () => {
+  test("should be 0 from December 25", () => {
     const dec25 = new Date(2000, 12 - 1, 25);
     expect(daysUntilChristmas(dec25)).to.equals(0);
+  });
+
+  test("should be 364 from December 26 when the next year is not a leap year", () => {
+    const dec26 = new Date(2000, 12 - 1, 26);
+    expect(daysUntilChristmas(dec26)).to.equals(364);
+  });
+
+  test("should be 365 from December 26 when the next year is a leap year", () => {
+    const dec26 = new Date(2003, 12 - 1, 26);
+    expect(daysUntilChristmas(dec26)).to.equals(365);
   });
 });
