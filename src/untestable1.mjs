e1c3c8d71c84b5ebc343ad795bb7ddfb034f09cf
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-10-09 15:19:45 +0300

    Add comments on testability. Add refactored copy

--- src/untestable1.mjs
+++ src/untestable1.mjs
@@ -1,4 +1,9 @@
+// Milliseconds per a day is a global variable for no reason because it is not used anywhere.
+// It is not a real constant and could be changed anytime so it is best to define it inside the function. 
+
 const millisPerDay = 24 * 60 * 60 * 1000;
+
+// Getting the current time makes testing impossible because we cannot control it without changing system settings
 
 export function daysUntilChristmas() {
   const now = new Date();
