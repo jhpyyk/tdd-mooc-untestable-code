627c869abdbcad555d97499a77dd41916aff2df9
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-10-17 16:02:40 +0300

    Add test

--- test/untestable2.test.mjs
+++ test/untestable2.test.mjs
@@ -3,14 +3,20 @@ import { expect } from "chai";
 import { diceHandValue, diceRoll } from "../src/untestable2_copy.ts";
 
 describe("Untestable 2: a dice game", () => {
-	const rand = () => {
-		return 0.0;
-	};
 	test("todo", () => {
 		// TODO: write proper tests
 		expect(diceHandValue()).to.be.a("number");
 	});
 	test("diceRoll returns 1 on rand(0.0)", () => {
+		const rand = () => {
+			return 0.0;
+		};
 		expect(diceRoll(rand)).to.equals(1);
+	});
+	test("diceRoll returns 6 on rand(0.99)", () => {
+		const rand = () => {
+			return 0.99;
+		};
+		expect(diceRoll(rand)).to.equals(6);
 	});
 });
